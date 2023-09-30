c4a098b69f44f4dba68b955f9f2553f95f870417
Tyler Jankowski <tyler.jankowski@hotmail.com>
2023-09-30 18:10:05 -0400

    update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,43 +1,11 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { userListReducer, userLoginReducer, userDeleteReducer, userRegisterReducer, userDetailsReducer, userUpdateReducer } from './reducers/userReducers'
-import { contactRequestDetailsReducer, contactRequestListReducer, contactRequestUpdateReducer } from './reducers/contactRequestReducers'
-import { clientCompaniesListReducer, clientCompanyCreateReducer, clientCompanyDeleteReducer, clientCompanyDetailReducer, clientCompanyListOptionsReducer, clientCompanyUpdateReducer } from './reducers/clientCompaniesReducer'
-import { invoiceCreateReducer, invoiceDetailReducer, invoiceListReducer, invoiceUpdateReducer } from './reducers/invoiceReducers'
 
 const reducer = combineReducers({
-    userLogin: userLoginReducer,
-    userList: userListReducer,
-    userDelete: userDeleteReducer,
-    userRegister: userRegisterReducer,
-    userDetail: userDetailsReducer,
-    userUpdate: userUpdateReducer,
-    
-    contactRequestList: contactRequestListReducer,
-    contactRequestDetail: contactRequestDetailsReducer,
-    contactRequestUpdate: contactRequestUpdateReducer,
-
-    clientCompanyList: clientCompaniesListReducer,
-    clientCompanyCreate: clientCompanyCreateReducer,
-    clientCompanyDetail: clientCompanyDetailReducer,
-    clientCompanyUpdate: clientCompanyUpdateReducer,
-    clientCompanyDelete: clientCompanyDeleteReducer,
-
-    clientCompanyListOptions: clientCompanyListOptionsReducer,
-
-    invoiceList: invoiceListReducer,
-    invoiceCreate: invoiceCreateReducer,
-    invoiceDetail: invoiceDetailReducer,
-    invoiceUpdate: invoiceUpdateReducer,
     
 })
-
-const userInfoFromStorage = localStorage.getItem('userInfo') ? 
-    JSON.parse(localStorage.getItem('userInfo')) : null
-
 const initialState = {
-    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
